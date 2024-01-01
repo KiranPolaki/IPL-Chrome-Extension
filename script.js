260a9d8afde0722c1bd52f6fdda377839f35d90b
polakikiran <polakisaikiran2@gmail.com>
2024-01-02 02:15:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent";
+const urlSched =
+  "https://cricbuzz-cricket.p.rapidapi.com/schedule/v1/international";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "67c58b0aecmshf86f64500820ec9p182073jsn310dcd258272",
+    "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
+  },
+};
+
+fetch(urlSched, options)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    // if (data.status != "success") return;
+    if (!matchList) return [];
+    const matchList = data.data;
+    console.log(matchList);
+  });
